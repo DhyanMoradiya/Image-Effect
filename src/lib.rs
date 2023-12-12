8eba2f7d02821d158128c674441e6a7a0464056e
Dhyan <mrkingmoradiya@gmail,com>
2023-12-12 16:52:51 +0530

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -7,23 +7,17 @@ use image::ImageOutputFormat::Png;
 
 #[wasm_bindgen]
 pub fn grayScale(encoded_file : &str) -> String {
-    log(&"GreyScale method is called".into());
 
     let string_to_vector_image = STANDARD.decode(&encoded_file).unwrap();
-    log(&"Image decoded".into());
 
     let mut img = load_from_memory(&string_to_vector_image).unwrap();
-    log(&"Image genrated".into());
 
     img = img.grayscale();
-    log(&"gray scale effect applied".into());
 
     let mut img_buffer= vec![];
     img.write_to(&mut img_buffer, Png).unwrap();
-    log(&"Image stored in to buffer".into());
 
     let  encoded_img = STANDARD.encode(img_buffer);
-    log(&"image encoded".into());
 
     let data_url = format!("data:image/png;base64,{}", encoded_img);
     return data_url;
